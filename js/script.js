94a1d39dd96c8e9fad7ca5f7117e50e619f3d15b
Fabrizio <whoet.wo@gmail.com>
2020-10-22 15:15:19 +0200

    Esercizio dadi completato

--- js/script.js
+++ js/script.js
@@ -12,3 +12,22 @@ for (var i = 0; i < emailList.length; i++){
 }
 
 message.innerHTML = feedback;
+
+//Fine primo esercizio
+
+//Inizio secondo esercizio
+
+var userWinner = document.getElementById('winner');
+
+var giocatore = Math.floor(Math.random() * 6)+1;
+var computer = Math.floor(Math.random() * 6)+1;
+
+if (giocatore > computer){
+   userWinner.innerHTML = "Complimenti giocatore, hai vinto!"
+}else if (giocatore == computer){
+   userWinner.innerHTML = "Avete pareggiato!"
+}else {
+   userWinner.innerHTML = "Mi dispiace, ha vinto il computer!"
+}
+
+console.log(giocatore, computer)
